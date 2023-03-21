67c158779c4bfde4c5cb3b3d8708378c311ab944
danusorn23456 <danusorn23456@gmail.com>
2023-03-21 15:01:06 +0700

    add single file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require('path');
+const fs = require('fs');
+const multer = require('multer')
 const imageApi = require("./api/image");
 //#endregion
 
@@ -19,7 +21,73 @@ app.get('/', (req, res) => {
   res.send('Welcome to image holder api')
 })
 
-app.use("/api/image",imageApi)
+//#region multer setup
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, './public')
+  },
+  filename: (req, file, cb) => {
+      cb(null,file.originalname)}
+})
+
+// const upload = multer({ storage: storage })
+
+const multi_upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+          cb(null, true);
+      } else {
+          cb(null, false);
+          const err = new Error('Only .png, .jpg and .jpeg format allowed!')
+          err.name = 'ExtensionError'
+          return cb(err);
+      }
+  },
+}).array('uploadedImages', 12)
+//#endregion
+
+app.get("api/image/", (req, res) => {
+  const directoryPath = path.join(__dirname, "/public");
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return res.status(500).send('Error getting images.');
+    }
+    const images = files.filter(file => {
+      const regex = /\.(jpg|jpeg|png|gif)$/i;
+      return regex.test(file);
+    }).map(file => {
+      return {
+        filename: file,
+        src: `${req.protocol}://${req.hostname}:${port}/public/${file}`
+      };
+    });
+    res.send(images);
+  });
+})
+
+app.post("api/image/uploads",(req,res)=>{
+    multi_upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            res.status(500).send({ error: { message: `Multer uploading error: ${err.message}` } }).end();
+            return;
+        } else if (err) {
+            // An unknown error occurred when uploading.
+            if (err.name == 'ExtensionError') {
+                res.status(413).send({ error: { message: err.message } }).end();
+            } else {
+                res.status(500).send({ error: { message: `unknown uploading error: ${err.message}` } }).end();
+            }
+            return;
+        }
+
+        // Everything went fine.
+        // show file `req.files`
+        // show body `req.body`
+        res.status(200).end('Your files uploaded.');
+    })
+})
 
 //#region final command
 app.listen(port, () => {
